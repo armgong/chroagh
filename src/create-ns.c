e0d47bfb72fb17b89a4abf1955ad8a84c5861233
Nicolas Boichat <nicolas@boichat.ch>
2013-08-25 14:58:08 +0800

    Working namespace creation, integrated into enter-chroot, we can "boot" Arch!

--- src/create-ns.c
+++ src/create-ns.c
@@ -1,15 +1,21 @@
-/* Implements the container interface found in http://www.freedesktop.org/wiki/Software/systemd/ContainerInterface/  */
-/* Inspired from clone man page, as well as systemd-nspawn. */
-/* Good documentation here: https://lwn.net/Articles/532271/ */
-/* gcc create-ns.c -o create-ns -static -Wall -O2 */
-/* sudo ./create-ns /usr/local/chroots/xalarm /lib/systemd/systemd */
+/* Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Implements the container interface found at
+ * http://www.freedesktop.org/wiki/Software/systemd/ContainerInterface/
+ * Inspired from clone man page, as well as systemd-nspawn.
+ * Good documentation here: https://lwn.net/Articles/532271/
+ *
+ * gcc create-ns.c -o create-ns -static -Wall -O2
+ */
 
 #define _GNU_SOURCE
 #include <sys/stat.h>
 #include <sys/prctl.h>
 #include <sys/capability.h>
 #include <sys/wait.h>
-#include <sys/mount.h>
+#include <sys/ioctl.h>
 #include <sched.h>
 #include <string.h>
 #include <stdio.h>
@@ -18,8 +24,9 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <termios.h>
+#include <poll.h>
 
-static int verbose = 3;
+static int verbose = 0;
 
 #define log(level, str, ...) do { \
     if (verbose >= (level)) printf("%s: " str "\r\n", __func__, ##__VA_ARGS__); \
@@ -28,44 +35,14 @@ static int verbose = 3;
 #define error(str, ...) printf("%s: " str "\r\n", __func__, ##__VA_ARGS__)
 
 /* Similar to perror, but prints function name as well */
-#define syserror(str, ...) do { \
+#define syserror(str, ...) \
     printf("%s: " str " (%s)\r\n", \
-                    __func__, ##__VA_ARGS__, strerror(errno)); \
-    exit(1); \
-} while (0)
+                    __func__, ##__VA_ARGS__, strerror(errno));
 
 static int ptfd = -1;
 
-static int mountchroot(const char* root, const char *source, const char *target,
-                 const char *filesystemtype, unsigned long mountflags,
-                 const void *data) {
-    char newtarget[128];
-    snprintf(newtarget, 128, "%s%s", root, target);
-    log(3, "Mounting %s to %s (%s)", source, newtarget, data);
-    return mount(source, newtarget, filesystemtype, mountflags, data);
-}
-
-static int devmknod(char* root, char *in, char* out) {
-    char oldfn[128];
-    char newfn[128];
-    struct stat st;
-    snprintf(oldfn, 128, "%s", in);
-    snprintf(newfn, 128, "%s%s", root, out ? out : in);
-
-    stat(oldfn, &st);
-
-    return mknod(newfn, st.st_mode, st.st_rdev);
-}
-
 static int childFunc(void *arg) {
-    char **argv = arg;
-    char* env[] = {
-        "PATH=/usr/sbin:/usr/bin",
-        "container=crouton", NULL};
-
-    char* root = argv[0];
-    char** init = &argv[1];
-
+    char** init = arg;
     char* name = strdup(ptsname(ptfd));
 
     log(3, "ptfd=%d|%s", ptfd, name);
@@ -76,50 +53,31 @@ static int childFunc(void *arg) {
     close(ptfd);
 
     int slavept = open(name, O_RDWR);
-    fprintf(stderr, "slavept=%d\n", slavept);
-    dup2(STDIN_FILENO, STDOUT_FILENO);
-    fprintf(stdout, "stdout\n");
-    dup2(STDIN_FILENO, STDERR_FILENO);
-    fprintf(stderr, "stderr\n");
+    if (slavept != STDIN_FILENO) {
+        syserror("slavept (%d) != 0", slavept);
+        return 1;
+    }
+    if (dup2(STDIN_FILENO, STDOUT_FILENO) < 0 ||
+        dup2(STDIN_FILENO, STDERR_FILENO) < 0) {
+        syserror("dup2");
+        return 1;
+    }
 
-    setsid();
+    if (setsid() < 0) {
+        syserror("setsid");
+        return 1;
+    }
 
-    prctl(PR_SET_PDEATHSIG, SIGKILL);
+    if (prctl(PR_SET_PDEATHSIG, SIGKILL) < 0) {
+        syserror("prctl");
+        return 1;
+    }
 
     /* start cgroup */
     // Maybe not needed for now...
 
-    /* Mount needed stuff */
-    mountchroot(root, "proc", "/proc", "proc", 0, NULL);
-    mountchroot(root, "/proc/sys", "/proc/sys", NULL, MS_BIND, NULL);
-    mountchroot(root, NULL, "/proc/sys", NULL, MS_BIND|MS_RDONLY|MS_REMOUNT, NULL);
-    mountchroot(root, "sysfs", "/sys", "sysfs", MS_RDONLY, NULL);
-    mountchroot(root, "tmpfs", "/dev", "tmpfs", 0, "mode=755");
-    char buf[128];
-    snprintf(buf, 128, "%s/dev/pts", root);
-    mkdir(buf, 0755);
-    /* newinstance does not work? */
-    mountchroot(root, "devpts", "/dev/pts", "devpts", 0, "newinstance,ptmxmode=0666,mode=620,gid=5");
-
-    devmknod(root, "/dev/null", NULL);
-    devmknod(root, "/dev/zero", NULL);
-    devmknod(root, "/dev/full", NULL);
-    devmknod(root, "/dev/random", NULL);
-    devmknod(root, "/dev/urandom", NULL);
-    devmknod(root, "/dev/tty", NULL);
-    devmknod(root, "/dev/ptmx", NULL);
-//    devmknod(root, name, "/dev/console");
-/*    snprintf(buf, 128, "%s/dev/ptmx", root);
-      symlink("pts/ptmx", buf);*/
-    snprintf(buf, 128, "%s/dev/console", root);
-    symlink(name, buf);
-
-    if (chroot(root) < 0) {
-        syserror("chroot");
-    }
-
-    chdir("/");
-
+    /* We can do without dropping those caps... */
+#if 0
     /* Drop CAP_MKNOD from children as well */
     prctl(PR_CAPBSET_DROP, CAP_MKNOD);
     prctl(PR_CAPBSET_DROP, CAP_SETPCAP);
@@ -128,27 +86,32 @@ static int childFunc(void *arg) {
     cap_user_data_t datap;
 
     hdrp = malloc(8);
-    datap = malloc(3*4);
+    datap = malloc(3*4*2);
 
     hdrp->pid = 0;
-    hdrp->version = _LINUX_CAPABILITY_VERSION;
+    hdrp->version = _LINUX_CAPABILITY_VERSION_2;
 
-    if (capget(hdrp, datap) < 0)
+    if (capget(hdrp, datap) < 0) {
         syserror("capget");
+        return 1;
+    }
 
     int caps = (1 << CAP_MKNOD) | (1 << CAP_SETPCAP);
     datap->permitted &= ~caps;
     datap->effective &= ~caps;
     datap->inheritable &= ~caps;
 
-    if (capset(hdrp, datap) < 0)
+    if (capset(hdrp, datap) < 0) {
         syserror("capset");
+        return 1;
+    }
+#endif
 
     log(3, "execvpe!");
 
-    execvpe(init[0], init, (char**)env);
+    execvp(init[0], init);
     syserror("execvpe");
-    exit(1);
+    return 1;
 }
 
 #define STACK_SIZE (1024 * 1024) /* Stack size for cloned child */
@@ -160,8 +123,8 @@ int main(int argc, char** argv) {
     int termios_saved_valid = 0;
     struct termios termios_saved, termios_raw;
 
-    if (argc < 3) {
-        error("Need 2 args");
+    if (argc < 2) {
+        error("Need at least 1 arg.");
         exit(1);
     }
 
@@ -177,53 +140,82 @@ int main(int argc, char** argv) {
     /* Make a new pt */
     ptfd = posix_openpt(O_RDWR|O_NOCTTY|O_CLOEXEC|O_NDELAY);
 
+    if (ptfd < 0) {
+        syserror("posix_openpt");
+        goto exit;
+    }
+
     ioctl(STDIN_FILENO, TIOCGWINSZ, &ws);
     ioctl(ptfd, TIOCGWINSZ, &ws);
 
-    unlockpt(ptfd);
+    if (unlockpt(ptfd) < 0) {
+        syserror("unlockpt");
+        goto exit;
+    }
 
     stack = malloc(STACK_SIZE);
-    if (!stack)
+    if (!stack) {
         syserror("malloc");
+        goto exit;
+    }
 
     int fl = CLONE_NEWPID | CLONE_NEWNS | SIGCHLD;
     fl |= CLONE_NEWIPC | CLONE_NEWUTS;
 
     pid = clone(childFunc, stack+STACK_SIZE,
                 fl, &argv[1]);
-    if (pid == -1)
+    if (pid == -1) {
         syserror("clone");
+        goto exit;
+    }
     log(3, "clone() returned %ld", (long) pid);
 
     /* Add non-blocking flag */
     int flags = fcntl(ptfd, F_GETFL, 0);
     if (flags < 0 || fcntl(ptfd, F_SETFL, flags | O_NONBLOCK) < 0) {
         syserror("error in fnctl GETFL/SETFL.");
+        goto exit;
     }
 
     flags = fcntl(STDIN_FILENO, F_GETFL, 0);
     if (flags < 0 || fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK) < 0) {
         syserror("error in fnctl GETFL/SETFL.");
+        goto exit;
     }
 
     char buffer[1024];
-
     pid_t wait_pid;
 
+    struct pollfd fds[2];
+    fds[0].events = POLLIN;
+    fds[0].fd = STDIN_FILENO;
+    fds[1].events = POLLIN;
+    fds[1].fd = ptfd;
+
+    /* FIXME: Use SIGCHLD instead of timeout */
     while (1) {
         wait_pid = waitpid(pid, NULL, WNOHANG);
 
-        int n = read(ptfd, buffer, 1024);
-        if (n > 0) {
-            log(4, "read fd=%d n=%d", ptfd, n);
-            buffer[n] = 0;
-            write(STDOUT_FILENO, buffer, n);
+        int polln = poll(fds, 2, (waitpid == pid) ? 0 : 100);
+
+        if (polln < 0) {
+            syserror("poll");
+            goto exit;
         }
 
-        n = read(STDIN_FILENO, buffer, 1024);
-        if (n > 0) {
-            log(4, "read fd=0 n=%d", n);
-            write(ptfd, buffer, n);
+        if (fds[0].revents & POLLIN) {
+            int n = read(STDIN_FILENO, buffer, 1024);
+            if (n > 0) {
+                log(4, "read fd=0 n=%d", n);
+                write(ptfd, buffer, n);
+            }
+        }
+        if (fds[1].revents & POLLIN) {
+            int n = read(ptfd, buffer, 1024);
+            if (n > 0) {
+                log(4, "read fd=%d n=%d", ptfd, n);
+                write(STDOUT_FILENO, buffer, n);
+            }
         }
 
         if (wait_pid == -1) {
@@ -237,8 +229,10 @@ int main(int argc, char** argv) {
 
     log(3, "child has terminated");
 
+exit:
     if (termios_saved_valid) {
         tcsetattr(STDIN_FILENO, TCSANOW, &termios_saved);
     }
+    /* FIXME: Return child status */
     return 0;
 }
