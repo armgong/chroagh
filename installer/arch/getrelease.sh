04a6e130dd31c9a31423719c2604189fc11f74a7
Nicolas Boichat <nicolas@boichat.ch>
2013-10-28 19:05:18 +0800

    arch: getrelease.sh: support for architecture detection

--- installer/arch/getrelease.sh
+++ installer/arch/getrelease.sh
@@ -6,19 +6,32 @@
 # Grabs the release from the specified chroot ($1) and prints it out on stdout.
 # Fails with an error code of 1 if the chroot does not belong to this distro.
 
-if [ -z "$1" ]; then
-    echo "Usage: ${0##*/} chroot" 1>&2
+USAGE="${0##*/} -a|-r /path/to/chroot
+
+Detects the release (-r) or arch (-a) of the chroot and prints it on stdout.
+Fails with an error code of 1 if the chroot does not belong to this distro."
+
+if [ "$#" != 2 ] || [ "$1" != '-a' -a "$1" != '-r' ]; then
+    echo "$USAGE" 1>&2
     exit 2
 fi
 
-os="${1%/}/etc/os-release"
+rel="`sed -n -e 's/^ID=//p' "${2%/}/etc/os-release"`"
 
-if grep -q '^ID=arch$' "$os" 2>/dev/null; then
-    echo "arch"
-    exit 0
-elif grep -q '^ID=archarm$' "$os" 2>/dev/null; then
-    echo "alarm"
-    exit 0
+if [ "$rel" = 'archarm' ]; then
+    rel="alarm"
+elif [ "$rel" != 'arch' ]; then
+    exit 1
 fi
 
-exit 1
+# Print the architecture if requested
+if [ "$1" = '-a' ]; then
+    for pacmandesc in "${2%/}"/var/lib/pacman/local/pacman-[0-9]*/desc; do
+        awk 'ok {print; exit}  /^%ARCH%$/ {ok=1}' "$pacmandesc" 2>/dev/null
+        exit 0
+    done
+else
+    echo "$rel"
+fi
+
+exit 0
